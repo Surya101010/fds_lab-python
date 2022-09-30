918d8ca5f8c73e29ed6fa4d12b9ec38ac194107e
surya <greatuchsurya@gmail.com>
2022-09-30 10:02:53 +0530

    completed numpy file1

--- pandas.py
+++ pandas.py
@@ -1,1 +1,7 @@
-print("hello world")
+import numpy as np
+arr= np.array([[1,2,3],[4,2,5]])
+print("array is of type ",type(arr))
+print("no of dimensions:",arr.ndim)
+print("shape of array",arr.shape)
+print("size of array:",arr.size)
+print("array stores elements of type:",arr.dtype)
