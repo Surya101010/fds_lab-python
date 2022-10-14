25747c9e5f764bd3e43001fed45e90e10b569721
surya <greatuchsurya@gmail.com>
2022-10-14 11:08:35 +0530

    matplot3

---
+++ matplotlib3.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+x=np.arange(0,3*np.pi,0.1)
+y_sin=np.sin(x)
+y_cos=np.cos(x)
+plt.plot(x,y_sin)
+plt.plot(y_cos)
+plt.xlabel('x axis label')
+plt.ylabel('y axis label')
+plt.title('sine and cosine')
+plt.legend(['sine','cosine'])
+plt.show()
