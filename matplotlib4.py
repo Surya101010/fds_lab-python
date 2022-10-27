c5f0e05368261c3af4b6836bc198224b530f75d3
surya <greatuchsurya@gmail.com>
2022-10-27 13:22:30 +0530

    4th exp matplotlib

---
+++ matplotlib4.py
@@ -0,0 +1,19 @@
+from matplotlib import pyplot as plt
+x=[5,2,9,4,7]
+y=[10,5,8,4,2]
+plt.plot(x,y)
+plt.show()
+#from matplotlib import pyplot as plt
+x=[5,2,9,4,7]
+y=[10,5,8,4,2]
+plt.bar(x,y)
+plt.show()
+#from matplotlib import pyplot as plt
+y=[10,5,8,4,2]
+plt.hist(y)
+plt.show()
+#from matplotlib import pyplot as plt
+x=[5,2,9,4,7]
+y=[10,5,8,4,2]
+plt.scatter(x,y)
+plt.show()
